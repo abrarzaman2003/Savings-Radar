1e177a851c9ec35d5d9cc303b514878d5f9d3105
Abrar Zaman <abrarzaman2003@gmail.com>
2022-11-12 17:40:32 -0600

    made the map center on the current location spoofed by the emulator

--- SavingsRadar/App.js
+++ SavingsRadar/App.js
@@ -1,13 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {Platform, StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  // const [coords, setCoords] = useState([]);
+  const [loaded,setLoaded] = useState(false);
+  const [region, setRegion] = useState({
+    latitude: 51.5079145,
+    longitude: -96.0,
+    latitudeDelta: 0.75,
+    longitudeDelta: 0.75,
+  });
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      var lat = location["coords"].latitude;
+      var lon = location["coords"].longitude;
+      //var c = [lat,lon];
+      //setCoords(c);
+      //console.log("yo");
+      var newRegion = {
+        latitude: lat,
+        longitude: lon,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }
+      setRegion(newRegion);
+      setLoaded(true);
+    })();
+  }, [location]);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
       <Text>to start working on your app!</Text>
-      <MapView style={styles.map} />
+      {loaded ? <MapView style={styles.map} initialRegion={region}
+        //onRegionChangeComplete={(region) => setRegion(region)}
+        /> : <Text>"laoding"</Text>}
       <StatusBar style="auto" />
+      <Text>{text}</Text>
       
     </View>
   );
