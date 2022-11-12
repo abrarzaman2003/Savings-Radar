03580e12356efd2697042bc1f5fc2f58a4c2059d
SameerIslam <islamsameer91@gmail.com>
2022-11-12 17:55:24 -0600

    functioning markers

--- SavingsRadar/App.js
+++ SavingsRadar/App.js
@@ -1,14 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+  const macys = {
+    latitude: 32,
+    longitude: -96,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
+  const walmart = {
+    latitude: 33,
+    longitude: -94,
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+  };
+  const forever21 = {
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+  };
+  const homedepot = {
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+  };
   return (
     <View style={styles.container}>
-      <Text>to start working on your app!</Text>
-      <MapView style={styles.map} />
-      <StatusBar style="auto" />
-      
+      <MapView 
+        style={styles.map} 
+        initialRegion={macys}
+      >
+        <Marker coordinate={macys} pinColor='blue' title={"macys"} description={"its macys lmao"}/>
+        <Marker coordinate={walmart} pinColor='blue' />
+      </MapView>
     </View>
   );
 }
@@ -18,10 +45,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start',
   },
   map: {
-    width: Dimensions.get('window').width/2,
+    width: Dimensions.get('window').width,
     height: Dimensions.get('window').height/2,
   },
 });
