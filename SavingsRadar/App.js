86f8454fce60a06780325a66f68ce0e32c609721
Aarian <aarianahsan@gmail.com>
2022-11-12 23:23:42 -0600

    deleted sample assets

--- SavingsRadar/App.js
+++ SavingsRadar/App.js
@@ -3,15 +3,6 @@ import { StyleSheet, Text, View, Dimensions } from "react-native";
 import MapView from "react-native-maps";
 import MainContainer from "./navigation/MainContainer";
 
-/*
-    <View style={styles.container}>
-      
-      <MapView style={styles.map} />
-      <StatusBar style="auto" />
-      
-    </View>
-*/
-
 export default function App() {
   return <MainContainer />;
 }
