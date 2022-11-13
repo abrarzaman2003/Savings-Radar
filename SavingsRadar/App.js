09bb7561ae099521cea0c0352e6e92e6e20c95dd
Abrar Zaman <abrarzaman2003@gmail.com>
2022-11-13 04:02:02 -0600

    not

--- SavingsRadar/App.js
+++ SavingsRadar/App.js
@@ -1,57 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import {Platform, StyleSheet, Text, View, Dimensions, Button } from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
 
-import { config } from './config';
-
-import axios from 'axios';
-
-// returns a 2d array of arrays which contains arrays of coords, address
-export async function getResults(companyName,currentLat,currentLong){
-  var coordArr =[];
-  console.log("hi1");
-  const response = await axios({
-    method: 'get',
-    url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${companyName}&location=${currentLat}%2C${currentLong}&radius=15000&key=${config.APIKEY}`,
-  });
-  const x = await response.data.results;
-  for (var i =0; i<x.length; i++){
-    const b = await x[i].vicinity;
-    const a = await x[i].geometry.location;
-    console.log(a);
-    console.log("\n\n\n");
-    coordArr.push([a,b]);
-  } 
-  return coordArr;
-}
-function caller() {
-  getResults("macys", 32.98, -96.75).then((r)=>console.log(r));
-}
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
-  
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
       <Button
-        onPress={caller}
-        title="Learn More"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
+        title="Press to schedule a notification"
+        onPress={async () => {
+
+          await schedulePushNotification();
+        }}
       />
-      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: Dimensions.get('window').width/2,
-    height: Dimensions.get('window').height/2,
-  },
-});
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
+}
