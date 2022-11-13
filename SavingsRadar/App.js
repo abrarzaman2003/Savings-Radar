3a9b4d7e23310494c6c97baf458505ed52a01bc3
SameerIslam <islamsameer91@gmail.com>
2022-11-12 23:07:08 -0600

    map in card

--- SavingsRadar/App.js
+++ SavingsRadar/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
   const macys = {
@@ -10,8 +10,8 @@ export default function App() {
     longitudeDelta: 0.01,
   };
   const walmart = {
-    latitude: 33,
-    longitude: -94,
+    latitude: 32.001,
+    longitude: -96,
     latitudeDelta: 0,
     longitudeDelta: 0,
   };
@@ -29,12 +29,15 @@ export default function App() {
   };
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
       <MapView 
+        provider={PROVIDER_GOOGLE}
         style={styles.map} 
         initialRegion={macys}
+        
       >
         <Marker coordinate={macys} pinColor='blue' title={"macys"} description={"its macys lmao"}/>
-        <Marker coordinate={walmart} pinColor='blue' />
+        <Marker coordinate={walmart} pinColor='red' />
       </MapView>
     </View>
   );
@@ -45,10 +48,34 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    marginTop: 100,
     justifyContent: 'start',
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height/2,
+    width: Dimensions.get('window').width-75,
+    height: Dimensions.get('window').height/2 - 80,
+    borderRadius: 16,
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+    elevation: 9,
   },
+  cardContainer: {
+    width: 30,
+    backgroundColor: "#F2EFEA",
+    height: 20,
+    borderRadius: 16,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+    elevation: 9,
+  },
+  
 });
