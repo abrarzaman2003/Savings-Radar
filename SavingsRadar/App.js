55212006f3266baa48f20852047ef08b14db1f1a
Abrar Zaman <abrarzaman2003@gmail.com>
2022-11-12 23:41:28 -0600

    created the function to get all of the coordinates for the macys

--- SavingsRadar/App.js
+++ SavingsRadar/App.js
@@ -1,62 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import {Platform, StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
-import * as Location from 'expo-location';
+import {Platform, StyleSheet, Text, View, Dimensions, Button } from 'react-native';
+
+import { config } from './config';
+
+import axios from 'axios';
+
+export async function getResults(companyName,currentLat,currentLong){
+  var coordArr =[];
+  console.log("hi1");
+  const response = await axios({
+    method: 'get',
+    url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=${companyName}&location=${currentLat}%2C${currentLong}&radius=15000&key=${config.APIKEY}`,
+  });
+  const x = await response.data.results;
+  for (var i =0; i<x.length; i++){
+    const a = await x[i].geometry.location;
+    console.log(a);
+    console.log("\n\n\n");
+    coordArr.push(a);
+  } 
+  return coordArr;
+}
+function caller() {
+  getResults("macys", 32.98, -96.75).then((r)=>console.log(r));
+}
 
 export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  // const [coords, setCoords] = useState([]);
-  const [loaded,setLoaded] = useState(false);
-  const [region, setRegion] = useState({
-    latitude: 51.5079145,
-    longitude: -96.0,
-    latitudeDelta: 0.75,
-    longitudeDelta: 0.75,
-  });
-  useEffect(() => {
-    (async () => {
-      
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      var lat = location["coords"].latitude;
-      var lon = location["coords"].longitude;
-      //var c = [lat,lon];
-      //setCoords(c);
-      //console.log("yo");
-      var newRegion = {
-        latitude: lat,
-        longitude: lon,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      }
-      setRegion(newRegion);
-      setLoaded(true);
-    })();
-  }, [location]);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
+  
   return (
     <View style={styles.container}>
-      <Text>to start working on your app!</Text>
-      {loaded ? <MapView style={styles.map} initialRegion={region}
-        //onRegionChangeComplete={(region) => setRegion(region)}
-        /> : <Text>"laoding"</Text>}
-      <StatusBar style="auto" />
-      <Text>{text}</Text>
+      <Button
+        onPress={caller}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
       
     </View>
   );
