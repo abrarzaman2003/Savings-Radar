54b3bbdf4e77fc3189d5d5a29a67343d8e318ee0
Aarian <aarianahsan@gmail.com>
2022-11-12 21:23:09 -0600

    base

--- SavingsRadar/App.js
+++ SavingsRadar/App.js
@@ -1,27 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView from "react-native-maps";
+import MainContainer from "./navigation/MainContainer";
 
-export default function App() {
-  return (
+/*
     <View style={styles.container}>
-      <Text>to start working on your app!</Text>
+      
       <MapView style={styles.map} />
       <StatusBar style="auto" />
       
     </View>
-  );
+*/
+
+export default function App() {
+  return <MainContainer />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width/2,
-    height: Dimensions.get('window').height/2,
+    width: Dimensions.get("window").width / 2,
+    height: Dimensions.get("window").height / 2,
   },
 });
---
+++ SavingsRadar/navigation/MainContainer.js
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+// Screens
+import HomeScreen from "./screens/HomeScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+
+//Screen names
+const homeName = "Home";
+const detailsName = "Details";
+const settingsName = "Settings";
+
+const Tab = createBottomTabNavigator();
+
+function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (rn === detailsName) {
+              iconName = focused ? "list" : "list-outline";
+            } else if (rn === settingsName) {
+              iconName = focused ? "settings" : "settings-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "blue",
+          inactiveTintColor: "grey",
+          labelStyle: { paddingBottom: 10, fontSize: 10 },
+          style: { padding: 10, height: 70 },
+        }}
+      >
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={detailsName} component={DetailsScreen} />
+        <Tab.Screen name={settingsName} component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainContainer;
